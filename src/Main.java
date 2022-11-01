36e24dd03ec528b6177edf72e01e9ebb407e2e92
Stikender <nadik17@bk.ru>
2022-11-02 00:34:02 +0800

    finish exercise 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercise 1.1");
+
+        for (int a = 0; a <= 10; a ++) {
+            System.out.println(a);
+        }
+
+        System.out.println("Exercise 1.2");
+    }
+}
